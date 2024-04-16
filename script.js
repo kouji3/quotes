5ba87dc62c4b73554064feca080aff919090d611
Kouji <144450928+kouji3@users.noreply.github.com>
2024-04-16 15:35:02 +0200

    fixed the error: Missing defer in the script tag

--- script.js
+++ script.js
@@ -2,19 +2,20 @@ let inputQ = document.querySelector("#inputQ");
 let quotes = document.querySelector(".response");
 console.log(inputQ)
 
-inputQ.addEventListener("keydown", (event) => {
+inputQ.addEventListener("keydown", async (event) => {
     if(event.key === "Enter" && inputQ.value.toString().toLowerCase().trim() === "quote"){
-        fetch("https://animechan.xyz/api/random")
-          .then((response) => response.json())
-          .then((quote) => {
-            console.log(quote)
-            let quoteContainer = document.createElement("div");
-            quoteContainer.classList.add("res")
-            quoteContainer.innerText = `Quote: ${quote.quote}\n\nAnime: ${quote.anime}\n\nCharacter: ${quote.character}`;
 
-            quotes.appendChild(quoteContainer);
 
-          }); 
+        let result = await fetch("https://animechan.xyz/api/random");
+        let quote = await result.json();
+        console.log(quote)
+        let quoteContainer = document.createElement("div");
+        quoteContainer.classList.add("res")
+        quoteContainer.innerText = `Quote: ${quote.quote}\n\nAnime: ${quote.anime}\n\nCharacter: ${quote.character}`;
+
+        quotes.appendChild(quoteContainer);
+
+         
     }
 });
 
